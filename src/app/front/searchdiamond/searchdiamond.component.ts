307f2d0d799ca12cfdaf809567604403336d4338
dev-adistech <D@v@2020>
2020-03-18 14:18:13 +0530

    Merge branch 'master' of https://github.com/dev-adistech/narolagems
    # Conflicts:
    #	src/app/front/searchdiamond/searchdiamond.component.html

--- src/app/front/searchdiamond/searchdiamond.component.ts
+++ src/app/front/searchdiamond/searchdiamond.component.ts
@@ -639,6 +639,10 @@ export class SearchdiamondComponent implements OnInit {
       }else{
         this.searchResultAry = [];
       }
+
+
+      this.dataSource = this.searchResultAry;
+
     },errorSearchREs => {console.log('errorSearchREs ',errorSearchREs);});
 
   }
@@ -654,7 +658,8 @@ export class SearchdiamondComponent implements OnInit {
 
 
 
-  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
+  //columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
+    columnsToDisplay = ['LrHalf','PId','CertNo','S_Name','S_FullName','Carat','GQ_Name','GC_Name','GRate','Total','GCT_Name','GPO_Name','GSY_Name','Meas1','Meas2','Meas3','Meas','TotDepth','Table1','CAngle','PAngle','GFL_Name','CR_Name','Disc','TBIN_Name','BRN_Name','BS_SortName','BS_Name','SIN_Name','TIN_Name','SBIN_Name','SOIN_Name','EF_Name','EC_Name','BSHD_Name','GRap','IsPhoto','Ratio','SpecialDisc','GI_Date','GI_DateDay','IsFancy','ColorName','SZ_CODE','Loc_Name','FGRate','AMOUNT_NEW','GRATE_NEW','DISC_NEW','TSBIN_Name','KeyToSym','EVENTPKTNO','IsBPD','IsMovie','ISEXHIBITION','EXHIBITION_KEY','EXHIBITION_BY','NGS','origin','JanCat','OnlineDiscount','ExportBenefit','TotalBenefit'];
   // dataSource = [
   //   {
   //     position: 1,
@@ -737,87 +742,96 @@ export class SearchdiamondComponent implements OnInit {
   //   },
   // ];
 
-  dataSource = [
-    {
-      position: 1,
-      name: 'Hydrogen',
-      weight: 1.0079,
-      symbol: 'H',
-      description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-    }, {
-      position: 2,
-      name: 'Helium',
-      weight: 4.0026,
-      symbol: 'He',
-      description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`
-    }, {
-      position: 3,
-      name: 'Lithium',
-      weight: 6.941,
-      symbol: 'Li',
-      description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`
-    }, {
-      position: 4,
-      name: 'Beryllium',
-      weight: 9.0122,
-      symbol: 'Be',
-      description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`
-    }, {
-      position: 5,
-      name: 'Boron',
-      weight: 10.811,
-      symbol: 'B',
-      description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`
-    }, {
-      position: 6,
-      name: 'Carbon',
-      weight: 12.0107,
-      symbol: 'C',
-      description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`
-    }, {
-      position: 7,
-      name: 'Nitrogen',
-      weight: 14.0067,
-      symbol: 'N',
-      description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
-    }, {
-      position: 8,
-      name: 'Oxygen',
-      weight: 15.9994,
-      symbol: 'O',
-      description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`
-    }, {
-      position: 9,
-      name: 'Fluorine',
-      weight: 18.9984,
-      symbol: 'F',
-      description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`
-    }, {
-      position: 10,
-      name: 'Neon',
-      weight: 20.1797,
-      symbol: 'Ne',
-      description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`
-    },
-  ];
+  dataSource = [];
+
+  showDisplayName(param){
+
+      switch (param) {
+
+          case "PId":
+              return "Stone ID";
+              break;
+          case "IsBPD":
+              return "IsBPD";
+              break;
+          case "EVENTPKTNO":
+              return "Pkt No";
+              break;
+          case "S_Name":
+              return "Shape ";
+              break;
+          case "Carat":
+              return "Carat";
+              break;
+          case "GC_Name":
+              return "Color";
+              break;
+          case "GQ_Name":
+              return "Clarity";
+              break;
+          case "GCT_Name":
+              return "Cut";
+              break;
+          case "GPO_Name":
+              return "Polish";
+              break;
+          case "GSY_Name":
+              return "Sym.";
+              break;
+          case "GFL_Name":
+              return "Flo.";
+              break;
+          case "GRap":
+              return "Rap";
+              break;
+          case "Disc":
+              return "Disc.";
+              break;
+          case "GRate":
+              return "$/Ct.";
+              break;
+          case "CR_Name":
+              return "Lab";
+              break;
+          case "NGS":
+              return "NGS Comment";
+              break;
+          case "EC_Name":
+              return "Eye Clean ";
+              break;
+          case "BSHD_Name":
+              return "Shade";
+              break;
+          case "Meas":
+              return "Measurement";
+              break;
+          case "TotDepth":
+              return "Depth";
+              break;
+          case "Table1":
+              return "Table";
+              break;
+          case "Ratio":
+              return "Ratio";
+              break;
+          case "origin":
+              return "Origin";
+              break;
+          case "Loc_Name":
+              return "Location";
+              break;
+          case "CertNo":
+              return "Certificate No.";
+              break;
+
+
+
+          default:
+              return param;
+              break;
+      }
+
+  }
 
 
   //console.log(dataSource);
