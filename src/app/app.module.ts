3bc5831f2a65255b191e9cf88a16c82e5e05e25c
dev-adistech <D@v@2020>
2020-04-23 10:24:33 +0530

    fresh ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { MenuComponent } from './menu/menu.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NgforComponent } from './ngfor/ngfor.component';
-import { NgifComponent } from './ngif/ngif.component';
-import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent,
-    MenuComponent,
-    NgforComponent,
-    NgifComponent,
-    NgSwitchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
